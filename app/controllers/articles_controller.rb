a55c26d7ba034a698a87e8e035d43a00f2cebe3c
Callum Ahern <callum.ahern@simplybusiness.co.uk>
2021-12-02 14:51:54 +0000

    completed controller setup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def show
-    @article = Article.find(params[:id]) 
-    # article is a variable that is assigned to the result of (for example) Article.find(1), displaying the Article that has an id of 1
   end
 
   def index
@@ -13,8 +12,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+  end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save 
       flash[:notice] = "Article was created successfully"
       redirect_to articles_path(@article)
@@ -22,4 +24,28 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to articles_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id]) 
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
