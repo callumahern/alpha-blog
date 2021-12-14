c0dce7b33244889437c9a2e1a72db38ff7f917eb
Callum Ahern <callum.ahern@simplybusiness.co.uk>
2021-12-14 22:06:51 +0000

    added update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,40 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def new
     @user = User.new
   end
 
-  def create
-    byebug
+  def edit
+    @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User information has been updated"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
 
